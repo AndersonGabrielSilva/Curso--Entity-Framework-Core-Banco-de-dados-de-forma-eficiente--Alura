bcba79e5625471a6d1ca59438454845046fb5f0e
andersongabsilva <Andersongabriel_maria@hotmail.com>
2020-04-01 16:28:33 -0300

    Introdução ao EntityFramework, Passo a Passo, Contexto,UseSqlServer,.add() e .AddRange()

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
@@ -10,7 +11,46 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            //GravarUsandoEntity();
+            RecuperarProdutos();
+
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            Produto p1 = new Produto();
+            p1.Nome = "Harry Potter e a Ordem da Fênix";
+            p1.Categoria = "Livros";
+            p1.Preco = 19.89;
+
+            Produto p2 = new Produto();
+            p2.Nome = "Senhor dos Anéis 1";
+            p2.Categoria = "Livros";
+            p2.Preco = 19.89;
+
+            Produto p3 = new Produto();
+            p3.Nome = "O Monge e o Executivo";
+            p3.Categoria = "Livros";
+            p3.Preco = 19.89;
+
+          
+
+            using (var contexto = new LojaContext())
+            {
+                //contexto.Produtos.Add(p);
+                //contexto.Produtos.Add(p1);
+                //contexto.Produtos.Add(p2);
+                //contexto.Produtos.Add(p3);
+                //Ou podemos ultilizar o .AddRange
+                contexto.Produtos.AddRange(p1, p2, p3);
+                contexto.SaveChanges();
+            }
         }
 
         private static void GravarUsandoAdoNet()
