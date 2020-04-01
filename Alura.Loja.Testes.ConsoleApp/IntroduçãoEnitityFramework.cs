6dbef3db6a016ebccb968012e7fff91b7abbcb40
andersongabsilva <Andersongabriel_maria@hotmail.com>
2020-04-01 17:21:21 -0300

    **Teste Introdução EntityFramework

---
+++ Alura.Loja.Testes.ConsoleApp/IntroduçãoEnitityFramework.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class IntroduçãoEnitityFramework
+    {
+        /*INTRODUÇÃO AO ENTITY
+         * Por que o Entity?
+         * Por que atrazves dele fica muito mais facil realizar as manipulaçoes no banco de dados
+         * por que ele realiza a abstração dos comandos SQL no banco. Tornando a vida do Desenvolvedor
+         * mais facil e produtiva, pois ele não precisa se preoculpar tanto com os comandos SQL e se dedica
+         * mais ao desenvolvimento do codigo em si.
+         * Tambem ao ultilizar o Entity fica mais facil para o softwae evoluir pois ele é responsael por escrever,
+         * montar o parametro e converter os dados para o Banco.
+         * O Entity é um "Mapeamento Objeto Relacional (ORM)", ele não é o unico existe varios outros. Ele é apenas um deles.
+         * Mapeamento objeto-relacional é uma técnica de desenvolvimento utilizada para reduzir a impedância da 
+         * programação orientada aos objetos utilizando bancos de dados relacionais
+         */
+
+    }
+}
